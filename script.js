eaeb31d20c81caae06ecb1b3f94017d618ef29ec
joezinhoo77 <142986856+joezinhoo77@users.noreply.github.com>
2025-05-16 01:46:56 +0000

    aula 15/05

--- script.js
+++ script.js
@@ -30,6 +30,6 @@ document.addEventListener('DOMContentLoaded', function () {
     alternaContraste.addEventListener('click', function () {
         document.body.classList.toggle('alto-contraste')
     })
-
-
+    const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
 })
