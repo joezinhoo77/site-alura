8586e81e5e982edbcc93816cf032697baf51b3ad
joezinhoo77 <142986856+joezinhoo77@users.noreply.github.com>
2025-06-13 01:43:07 +0000

    AULA 9

--- script.js
+++ script.js
@@ -32,4 +32,9 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
 botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
-})
+});
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
